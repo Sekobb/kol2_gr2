f7a00e83768098010a747535ca45212847dc433d
Sekobb <majewski.jakub@tlen.pl>
2016-11-09 14:26:32 +0100

    Update kol2_gr2.py

--- kol2_gr2.py
+++ kol2_gr2.py
@@ -6,7 +6,7 @@
 # Make it possible to:
 # - Get students total average score (average across classes)
 # - get students average score in class
-# - hold students name and surname
+# - hold students name and surnames
 # - Count total attendance of student
 # The default interface for interaction should be python interpreter.
 # Please, use your imagination and create more functionalities. 
@@ -14,3 +14,23 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+
+from diary import *
+
+def main():
+	dziennik = diary()
+
+	dziennik.dodaj_studenta(student('Jan', 'Kowalski'))
+
+	(dziennik.stud(0)).dodaj_ocena_spr(5)
+	(dziennik.stud(0)).dodaj_ocena_spr(4)
+	(dziennik.stud(0)).dodaj_ocena_inna(4)
+
+	print(dziennik.studenci[0].srednia_ogolna())
+	print(dziennik.studenci[0].srednia_spr())
+
+
+
+if __name__ == "__main__":
+	main()
