a49889381c4e56252ef381b9ac24710a7a9ffa7d
Sekobb <majewski.jakub@tlen.pl>
2016-11-21 01:05:59 +0100

    Update diary.py

--- diary.py
+++ diary.py
@@ -1,15 +1,33 @@
-from student import *
+from __future__ import division
 
 class diary(object):
 
-	def __init__(self):
-		self.studenci = []
+	def __init__(self, przedmioty, studenci):
+		self.baza = {}
+		for p in przedmioty:
+			self.baza[p] = {}
+			for s in studenci:
+				self.baza[p][s] = {}
+				self.baza[p][s]['spr'] = []
+				self.baza[p][s]['inne'] = []
+				self.baza[p][s]['ob'] = []
 
-	def dodaj_studenta(self, student):
-		self.studenci.append(student)
 
-	def __add__(self, student):
-		self.studenci.append(student)
+	def dodaj(self, przedmiot, student, status, ocena):
+		self.baza[przedmiot][student][status].append(ocena)
 
-	def stud(self, i):
-		return self.studenci[i]
+	def srednia(self, przedmiot, student):
+		suma = sum(self.baza[przedmiot][student]['spr']) + sum(self.baza[przedmiot][student]['inne'])
+		ilosc = len(self.baza[przedmiot][student]['spr']) + len(self.baza[przedmiot][student]['inne'])
+		return round(suma/ilosc, 2)
+
+	def spr_srednia(self, przedmiot, student):
+		suma = sum(self.baza[przedmiot][student]['spr'])
+		ilosc = len(self.baza[przedmiot][student]['spr'])
+		return round(suma/ilosc, 2)
+
+	def obecnosc(self, przedmiot, student):
+		suma = sum(self.baza[przedmiot][student]['ob'])
+		ilosc = len(self.baza[przedmiot][student]['ob'])
+		return str(round((suma/ilosc) * 100, 2)) + '%'
+
